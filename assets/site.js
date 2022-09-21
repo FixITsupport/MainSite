986c22c43d2a6b19d57a2bd89def82a6d73b90e1
Marco Taddei <marco@webmdt.com>
2022-09-21 16:37:59 -0400

    Scroll to Top & Minor Padding to TXT

--- assets/site.js
+++ assets/site.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function () {
-    jQuery("a[href^='#']").click(function (e) {
+    /*Smooth Scrolling anchor tags*/
+    jQuery(".nav-item a[href^='#']").click(function (e) {
         e.preventDefault();
         var elem = jQuery(this).attr("href");
-
         jQuery("html, body").animate(
             {
                 scrollTop: jQuery(elem).offset().top - 70,
@@ -13,14 +13,27 @@ jQuery(document).ready(function () {
 });
 
 jQuery(window).scroll(function() {
+    /*Header Section BG/Opacity*/
     if (jQuery(this).scrollTop() > 1){
         jQuery('#header-section').addClass("header-post-scroll").removeClass("header-pre-scroll");
         jQuery('#header-logo-link-white').hide();
         jQuery('#header-logo-link-black').show();
-    }
-    else{
+    }else{
         jQuery('#header-section').addClass("header-pre-scroll").removeClass("header-post-scroll");
         jQuery('#header-logo-link-white').show();
         jQuery('#header-logo-link-black').hide();
     }
+    
+    /*Header Section BG/Opacity*/
+    if (jQuery(this).scrollTop() > 200){
+        jQuery("#scrollToTop-wrapper").show();
+    }else{
+        jQuery("#scrollToTop-wrapper").hide();
+    }
 });
+
+/*Scroll to Top*/
+function scrollToTopFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
